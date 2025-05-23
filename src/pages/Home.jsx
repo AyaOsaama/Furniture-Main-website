8ef40b65e703bbf827a3398bf6615e13d26bea97
Nada Ossama <118224738+NadaOssama2001@users.noreply.github.com>
2025-05-23 04:52:01 +0300

    edit home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,6 +19,7 @@ import { Link } from "react-router-dom";
 import i18n from "../i18n.js";
 import { api } from "../axios/axios";
 import TagsButtons from "../components/TageButton/TagesButton.jsx";
+import Diff from "../components/diff/diff.jsx";
 
 function Home() {
   const { t } = useTranslation("home");
@@ -173,7 +174,8 @@ function Home() {
     {t("noSectionsFound") || "No sections found."}
   </p>
 )}
-
+{/* Diff */}
+<Diff></Diff>
 
       {/* Services */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 my-12 px-4 text-center sm:text-left justify-items-center">
